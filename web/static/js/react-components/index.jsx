ec693a47998db203e440360ff768422aeb4f0ca1
looshi <davluzius@gmail.com>
2019-10-07 09:20:07 -0600

    Add lingui react example

--- web/static/js/react-components/index.jsx
+++ web/static/js/react-components/index.jsx
@@ -1,10 +1,14 @@
 import PropTypes from "prop-types";
 import { hot } from "react-hot-loader/root";
+import { Trans as T } from "@lingui/react";
 
 const App = ({ message }) => {
   return (
     <div>
-      <span>{message}</span>
+      <span>{message} </span>
+      <span>
+        <T>and @lingui/react is working</T>
+      </span>
     </div>
   );
 };
